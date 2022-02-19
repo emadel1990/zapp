06af9d210bfaec91fb83e069054359a140ab559e
Gabriel Britez <gbritez@goiar.com>
2022-02-19 17:53:02 -0300

    feature: Setup Tailwindcss

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1>hello world</h1>
+      <h1 className="text-3xl font-bold underline">hello world</h1>
       <h2>Develop branch</h2>
     </div>
   );
